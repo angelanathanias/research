2b9450c8b46f0dd36729148b1d1ddc67308ab3b1
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:37:54 +1200

    Update string_input.py

--- string_input.py
+++ string_input.py
@@ -4,6 +4,7 @@
 # date: September 2016
  
 print ("Welcome to your first data entry program\n")
+#The characters "\n" are used to separate out the print statements when they appear on the console
 user_name = input("Please enter your name...\n")
 user_yob = int(input("Please enter your year of birth...\n"))
  
