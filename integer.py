081a619e150615ded9dca5b92904e51d1f721f1b
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:26:29 +1200

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,15 @@
+Integer:
+
+x = 10
+y = 2465511454663471
+z = -3255522
+
+You can check types of variables by using type() function in www.python-online.com
+
+For example
+
+print(type(x))
+
+print(type(y))
+
+print(type(z))
