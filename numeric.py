f16df9294af37d0e35baffd3b3e602fb53f9ecb2
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:28:26 +1200

    Update and rename integer.py to numeric.py

---
+++ numeric.py
@@ -0,0 +1,45 @@
+#Integer:
+
+x = 10
+y = 2465511454663471
+z = -3255522
+
+You can check types of variables by using type() function in www.python-online.com
+
+For example
+
+print(type(x))
+
+print(type(y))
+
+print(type(z))
+
+#Float:
+
+x = 5.2
+y = -105.06
+
+z= 12e3
+
+Here z is a scientific number where e shows the power of 10
+You can check types of variables by using type() function in www.python-online.com
+
+For example:
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Complex:
+
+x=1j
+
+y=-3j
+
+z= complex(3,5)
+
+You can check types of variables by using type() function in www.python-online.com
+
+For example:
+print(type(x))
+print(type(y))
+
