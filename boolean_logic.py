f6fb070d969228748493a67e5a263e59c5c26895
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:43:58 +1200

    Create boolean_logic.py

---
+++ boolean_logic.py
@@ -0,0 +1,20 @@
+# BooleanAnd.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+is_alive = True
+time_remaining = False
+ 
+# Test case assertion 1: result should be False
+print("The game in progress status is....\n")
+print(is_alive
+      and time_remaining, "\n")
+ 
+print("Giving more time to play...\n")
+time_remaining = True
+ 
+# Test case assertion 2: result should be True
+print("The game in progress status is now....\n")
+print(is_alive
+      and time_remaining, "\n")
