6062287e4f3fbf8fc0a78ca1dee3a7052116d414
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 21:03:02 +1200

    Create simple_function.py
    to call a function

---
+++ simple_function.py
@@ -0,0 +1,15 @@
+# Function1.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+def show_hello():
+    print("Hello there, this is my very first function!!!\n\n")
+ 
+print("Testing my first user defined function...\n\n")
+ 
+# invoking the function
+show_hello()
+ 
+# invoking the function again
+show_hello()
