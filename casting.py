a61ce03f8b0a67c6f93e9a81cde484979da72f3d
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:33:11 +1200

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,26 @@
+# Casting.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+# variable set to integer
+my_score = 201
+ 
+print("The type of my_score is ", type(my_score),"\n")
+print("my_score is ", my_score,"\n")
+ 
+print("Casting to a string...\n")
+# variable cast to string
+my_score = str(my_score)
+ 
+print("The type of my_score is now ", type(my_score),"\n")
+print("my_score is ", my_score,"\n")
+The type of my_score is  <class 'int'> 
+ 
+my_score is  201 
+ 
+Casting to a string...
+ 
+The type of my_score is now  <class 'str'> 
+ 
+my_score is  201
