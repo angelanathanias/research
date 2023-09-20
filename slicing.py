9f417a61f386b5ed92fa463f7487497d612797cb
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-20 20:56:05 +1200

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,18 @@
+# ListsSlice.py
+#
+# @ author: A. N. Other
+# date: September 2016
+# empty list
+ 
+my_list = ['k','e','t','e','m','a','r','a','e']
+# elements 3rd to 5th
+print(my_list[2:5])
+ 
+# elements beginning to 4th
+print(my_list[:-5])
+ 
+# elements 6th to end
+print(my_list[5:])
+ 
+# elements beginning to end
+print(my_list[:])
