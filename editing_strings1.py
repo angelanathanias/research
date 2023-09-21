aa7fe80470de8c226ca11bc9f404b6464e897b4a
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:42:38 +1200

    Create editing_strings1.py
    Replacing part of a string with a new variable value.

---
+++ editing_strings1.py
@@ -0,0 +1,42 @@
+# String4.py
+# Replacing part of a string with a new variable value. 
+# @ author: A. N. Other
+# date: September 2016
+ 
+string_1 = "Hello World"
+ 
+# replacing part of a string
+print("Replacing part of a string...\n{0}"
+      .format(string_1.replace("Hello", "Goodbye")),
+      "\n")
+ 
+#Changing Upper and Lower Case Strings
+string_1 = "hElLo wOrlD"
+print("Making a string upper case...\n{0}"
+      .format(string_1.upper()),
+      "\n")
+ 
+print("Making a string lower case...\n{0}"
+      .format(string_1.lower()),
+      "\n")
+ 
+print("Making a string title case...\n{0}"
+      .format(string_1.title()),
+      "\n")
+ 
+print("Making a string capitalised...\n{0}"
+      .format(string_1.capitalize()),
+      "\n")
+ 
+print("Making a string swap case...\n{0}"
+      .format(string_1.swapcase()),
+      "\n")
+ 
+print("Reversing and inserting characters to a string...\n{0}"
+      .format("*".join(reversed(string_1))),
+      "\n")
+
+ 
+
+ Note - feel free to practice this process by replicating the example. Did you get a similar result?
+
