ff77b48c887c001f2dfb8b6d9633accbe9698ace
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:39:18 +1200

    Create searching_strings1.py

---
+++ searching_strings1.py
@@ -0,0 +1,9 @@
+# String1.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+string_1 = "Whanau!"
+ 
+print("The third letter is {0}\n".format(string_1[2]))
+print("The length of the string is {0}".format(len(string_1)))
