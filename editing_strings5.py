cdab9a59128210b64de24b186586393951150981
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:46:38 +1200

    Create editing_strings5.py
    making uppercase

---
+++ editing_strings5.py
@@ -0,0 +1,13 @@
+Making a string upper case...
+
+HELLO WORLD 
+
+#The upper() method converts all lowercase characters in a string
+
+#into uppercase characters.
+
+string_1 = "Hello World"
+
+print(‘Converting to uppercase')
+
+print(string_1.upper())
