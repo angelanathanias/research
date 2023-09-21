6482528712b75a7347f2809c529d287202ec8979
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:48:03 +1200

    Create splitting_strings.py

---
+++ splitting_strings.py
@@ -0,0 +1,17 @@
+# String5.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+string_1 = "It's only after we've lost everything " \
+           "that we're free to do anything"
+ 
+# splitting the string
+print("Splitting the string...\n{0}"
+      .format(string_1.split()),
+      "\n")
+ 
+# splitting the string on the letter e
+print("Splitting the string on the letter e...\n{0}"
+      .format(string_1.split("e")),
+      "\n")
