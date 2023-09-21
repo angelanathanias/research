cd6f97444b57e5a57c27c11966ca98f173021525
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:40:54 +1200

    Create searching_string3.py

---
+++ searching_string3.py
@@ -0,0 +1,7 @@
+string_1.isalnum()         #check if all char are alphanumeric 
+string_1.isalpha()         #check if all char in the string are alphabetic
+string_1.isdigit()         #test if string contains digits
+string_1.istitle()         #test if string contains title words
+string_1.isupper()         #test if string contains upper case
+string_1.islower()         #test if string contains lower case
+string_1.isspace()         #test if string contains spaces
