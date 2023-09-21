b7e117393dc9b16ab556726a0144d0912b55132a
angelanathanias <141601566+angelanathanias@users.noreply.github.com>
2023-09-21 20:44:33 +1200

    Create editing_strings3.py
    Concatenation - combining two or more strings into a single string

---
+++ editing_strings3.py
@@ -0,0 +1,17 @@
+# Concatenation
+ 
+# To concatenate strings in Python use the "+" operator.
+# "Hello " + "World" = "Hello World"
+# "Hello " + "World" + "!" = "Hello World!"
+ 
+string_1 = "Hello World"
+ 
+print("Using the join method to add a : between every char...\n{0}"
+      .format(":".join(string_1)),
+      "\n")
+ 
+print("Using the join method to add a whitespace between every char...\n{0}"
+      .format(" ".join(string_1)),
+      "\n")
+
+ 
